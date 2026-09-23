56cf69bbb0d3315a5906dc60b10e4c67478ea20c
claudiorogb <claudiorogb@gmail.com>
2026-09-23 10:09:01 -0300

    Remover texto adicional ao reiniciar envio

--- src/App.jsx
+++ src/App.jsx
@@ -4316,7 +4316,7 @@ function MessageSending({ organization, settings, userEmail }) {
         action === 'pause'
           ? 'Envio pausado. Clique em Reiniciar para continuar.'
           : action === 'resume'
-            ? 'Envio reiniciado. O botão voltou para Pausar.'
+            ? 'Envio reiniciado.'
             : 'Envio cancelado. As mensagens pendentes não serão enviadas.'
       )
       await loadData()
